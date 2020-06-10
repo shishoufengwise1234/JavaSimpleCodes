9a568d0319b1b7e7d19272e2f2a4418b709e5617
shishoufeng <shishoufeng@szy.cn>
2020-06-10 12:14:38 +0800

    dev:添加构建项目

---
+++ src/com/java_simple/codes/P.kt
@@ -0,0 +1,14 @@
+package com.java_simple.codes
+
+
+/**
+ * Created by shishoufeng on 2020/4/27.
+ *
+ * desc : 日志打印相关
+ *
+ *
+ */
+
+fun out(string: String){
+    println(string)
+}
---
+++ src/com/java_simple/codes/reflect/ReflectMain.java
@@ -0,0 +1,12 @@
+package com.java_simple.codes.reflect;
+
+import com.java_simple.codes.PKt;
+
+public class ReflectMain {
+
+
+    public static void main(String[] args) {
+
+        PKt.out("reflect main start");
+    }
+}
